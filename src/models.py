a638a4f3b5743c8355a962016468f083b12e3f97
NikitaPRV <nikitaprv@oxygen>
2025-02-05 22:10:16 +0300

    added router, remove create db

--- src/models.py
+++ src/models.py
@@ -1,12 +1,28 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DATETIME
 from src.database import Base
 
+
 class User(Base):
-    __tablename__ = "persons"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, unique=True, index=True)
-    last_name = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
-    city = Column(String, unique=True, index=True)
-    shirt_size = Column(String, unique=True, index=True)
+    password_hash = Column(String, index=True)
+    ip_address = Column(String, index=True)
+    created_at = Column(DATETIME, index=True)
+    last_login_time = Column(String, index=True)
+    country_code = Column(String, index=True)
+    phone = Column(String, index=True)
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    address = Column(String, index=True)
+    contact_phone = Column(String, index=True)
+    date_of_birth = Column(DATETIME, index=True)
--- src/schemas.py
+++ src/schemas.py
@@ -1,10 +1,25 @@
+from datetime import date
+
 from pydantic import BaseModel
 
 
 class User(BaseModel):
     id: int
+    username: str
+    email: str
+    password_hash: str
+    ip_address: str
+    created_at: date
+    last_login_time: str
+    country_code: str
+    phone: str
+
+
+class Customer(BaseModel):
+    id: int
+    user_id: int
     first_name: str
     last_name: str
-    email: str
-    city: str
-    shirt_size: str
+    address: str | None
+    contact_phone: str | None
+    date_of_birth: date | None
