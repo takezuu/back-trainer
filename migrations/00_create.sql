90c49174bb87024ee5fd642e32800bb5254b9cf5
NikitaPRV <nikitaprv@oxygen>
2025-02-07 19:58:53 +0300

    added create sql

--- migrations/00_create.sql
+++ migrations/00_create.sql
@@ -19,3 +19,26 @@ create table customers (
     contact_phone VARCHAR(20),
     date_of_birth DATE
 );
+
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL,
+    items_ids INT[] NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    discount FLOAT8 DEFAULT 0.0,
+    total_amount DECIMAL(10,2) NOT NULL,
+    status VARCHAR(50) DEFAULT 'pending',
+    delivery_address VARCHAR(200)
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    category VARCHAR(100),
+    item_color VARCHAR(20),
+    rating INT DEFAULT NULL
+);
