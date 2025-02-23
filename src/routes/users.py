ecd5201bcbd2e26337102a04f00c585a01bf6b8b
NikitaPRV <nikitaprv@oxygen>
2025-02-23 13:24:23 +0300

    added validation to user post cameback gitignore

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,11 +1,12 @@
 from typing import List, Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+
 from sqlmodel import Session
 from sqlmodel import select
+
 from src.schemas.users import UsersSchemas
 from src.models.users import UsersModels
 from src.database import get_session
-import re
 
 SessionDep = Annotated[Session, Depends(get_session)]
 router = APIRouter()
@@ -13,13 +14,13 @@ router = APIRouter()
 
 @router.get("/api/users", tags=["users"], response_model=List[UsersSchemas.Users])
 async def get_users(session: SessionDep,
-                     username: str = None,
-                     email: str = None,
-                     country_code: str = None,
-                     q: str = None,
-                     sort: str = "id",
-                     order_by: str = "asc",
-                     page: int = 1, limit: int = 25):
+                    username: str = None,
+                    email: str = None,
+                    country_code: str = None,
+                    q: str = None,
+                    sort: str = "id",
+                    order_by: str = "asc",
+                    page: int = 1, limit: int = 25):
     users = UsersModels.Users
     query = select(users)
 
@@ -50,14 +51,20 @@ async def get_user(user_id: int, session: SessionDep):
 
 @router.post("/api/users", tags=["users"], response_model=UsersSchemas.UserAdded)
 async def create_user(user: UsersModels.UserAdd, session: SessionDep):
+    print(user)
     db_user = UsersModels.Users(**user.model_dump())
 
     if db_user.phone:
-        phone_pattern = re.compile(r"\+[\d]{11, }")
-        result = phone_pattern.match(db_user.phone)
-        if result:
-            query = select(UsersModels.Users).where(UsersModels.Users.phone == db_user.phone)
-            phone_exists = session.exec(query).first() is not None
+        query = select(UsersModels.Users).where(UsersModels.Users.phone == db_user.phone)
+        phone_exists = session.exec(query).first() is not None
+        if phone_exists:
+            raise HTTPException(status_code=404, detail="Phone is already use")
+
+    if db_user.email:
+        query = select(UsersModels.Users).where(UsersModels.Users.email == db_user.email)
+        email_exists = session.exec(query).first() is not None
+        if email_exists:
+            raise HTTPException(status_code=404, detail="Email is already use")
 
     session.add(db_user)
     session.commit()
