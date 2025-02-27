d3934c853d3681e0919c0151db83e371150b148a
RomanBTG <>
2025-02-27 21:02:48 +0300

    Fix git

---
+++ src/dependencies/items.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from fastapi import Depends, HTTPException
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from src.database import get_session
+from src.models.items import ItemsModels
+
+
+async def item_exists(item_id: int, session: AsyncSession = Depends(get_session)):
+    query = select(ItemsModels.Items).where(ItemsModels.Items.id == item_id)
+    item = session.exec(query).first()
+    if item:
+        return item
+    raise HTTPException(status_code=404, detail="Item not found")
---
+++ src/dependencies/users.py
@@ -0,0 +1,14 @@
+from fastapi import Depends, HTTPException
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from src.database import get_session
+from src.models.users import UsersModels
+
+
+async def user_exists(user_id: int, session: AsyncSession = Depends(get_session)):
+    query = select(UsersModels.Users).where(UsersModels.Users.id == user_id)
+    user = session.exec(query).first()
+    if user:
+        return user
+    raise HTTPException(status_code=404, detail="User not found")
