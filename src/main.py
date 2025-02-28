1834a8eddac4b259d0dd623bfa7a43e7c51d8508
NikitaPRV <nikitaprv@oxygen>
2025-02-28 03:36:51 +0300

    override error handler

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, FileResponse
 from src.routes import users, orders, items
-from fastapi.responses import FileResponse
+
 
 HTML_PATH = "UI/Ui.html"
 
@@ -8,6 +10,7 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(orders.router)
 app.include_router(items.router)
+
 
 
 @app.get("/favicon.ico")
@@ -18,3 +21,26 @@ async def favicon():
 @app.get("/")
 def root():
     return FileResponse(HTML_PATH)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = exc.errors()
+    error_details = []
+    for error in errors:
+        error_type = error["type"]
+        error_location = error["loc"][1]
+        error_message = error["msg"]
+
+        if error_message == "JSON decode error":
+            ctx = error["ctx"]["error"]
+            error_details.append(f"Type: {error_type}, Position: {error_location} - {error_message} - {ctx}")
+        else:
+            error_details.append(f"Type: {error_type}, Field: {error_location} - {error_message}")
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={
+            "message": "Validation failed",
+            "details": error_details
+        }
+    )
